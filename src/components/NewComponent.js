99a51bcbb73930a8fb2bf24d57f0abd10e48f0fb
Senad Djerlek <senajerlek@gmail.com>
2022-08-05 19:31:54 +0200

    react pracitce

---
+++ src/components/NewComponent.js
@@ -0,0 +1,13 @@
+import getExp from "./NewComponentHelp";
+  
+function DisplayExpenses (props) {
+
+    return (
+       <getExp></getExp>
+    )
+}
+
+
+
+
+export default DisplayExpenses;
---
+++ src/components/NewComponentHelp.js
@@ -0,0 +1,14 @@
+
+function getExp(props) {
+    
+    return (
+        <div>
+        <div> <h2>{props.date} </h2></div>        
+        <div> {props.title}</div>
+        <div> {props.amount}</div>
+
+        </div>
+    )
+} 
+
+export default getExp;
