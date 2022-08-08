87ffc899bb8d8de1ecc622b977cb6cf60e1b70d8
Senad Djerlek <senajerlek@gmail.com>
2022-08-09 00:04:47 +0200

    react practice

--- src/components/NewComponent.js
+++ src/components/NewComponent.js
@@ -1,9 +1,10 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import './NewComponent.css'
 
 function DisplayExpenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -27,7 +28,7 @@ function DisplayExpenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
