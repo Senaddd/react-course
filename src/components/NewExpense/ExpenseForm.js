debd04ca7f3a501d4c031cf7ba3984e9fc6d4245
Senad Djerlek <senajerlek@gmail.com>
2022-09-02 00:52:09 +0200

    Child To Parent Component Communication

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setenteredTitle] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
   const [enteredAmount, setEnetredAmount] = useState("");
@@ -57,7 +57,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setenteredTitle("");
     setEnetredAmount("");
     setEnteredDate("");
